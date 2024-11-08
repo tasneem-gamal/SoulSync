a5cf9051fb3fbf100efbcf18a836b3d56d666c2e
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-08 13:11:26 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:soulsync/soul_sync.dart';
+
+void main() {
+  runApp(const SoulSync());
+}
+
---
+++ lib/soul_sync.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SoulSync extends StatelessWidget {
+  const SoulSync({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
