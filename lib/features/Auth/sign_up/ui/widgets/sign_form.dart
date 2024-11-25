e7dc6dd34e62dede26f7c5b35d95f71b51ea7d74
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-25 14:01:10 -0800

    refactor sign up

--- lib/features/Auth/sign_up/ui/widgets/sign_form.dart
+++ lib/features/Auth/sign_up/ui/widgets/sign_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:soulsync/core/widgets/custom_button.dart';
 import 'package:soulsync/core/widgets/custom_text_form_field.dart';
 import 'package:soulsync/features/Auth/login/ui/widgets/title_of_text_field.dart';
 
@@ -25,7 +26,9 @@ class SignForm extends StatelessWidget {
           SizedBox(height: 16.h,),
           const TitleOfTextField(title: 'Confirm Password'),
           const SizedBox(height: 12,),
-          CustomTextFormField( validator: (value){})
+          CustomTextFormField( validator: (value){}),
+          SizedBox(height: 40.h,),
+          const CustomButton(text: 'Sign Up'),
         ],
       )
     );
