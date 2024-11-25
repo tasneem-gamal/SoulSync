7c7295326e42173cb9ce3c2ba4dbc5d4acb773c6
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-25 13:06:24 -0800

    add controllers to login

--- lib/features/Auth/login/ui/widgets/login_form.dart
+++ lib/features/Auth/login/ui/widgets/login_form.dart
@@ -4,23 +4,36 @@ import 'package:soulsync/core/widgets/custom_button.dart';
 import 'package:soulsync/core/widgets/custom_text_form_field.dart';
 import 'package:soulsync/features/Auth/login/ui/widgets/title_of_text_field.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     super.key,
   });
 
   @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: formKey,
       child: Column(
         children: [
           const TitleOfTextField(title: 'Email Address',),
           const SizedBox(height: 12,),
-          CustomTextFormField( validator: (value){}),
+          CustomTextFormField(
+            controller: emailController,
+            validator: (value){}
+          ),
           SizedBox(height: 16.h,),
           const TitleOfTextField(title: 'Password'),
           const SizedBox(height: 12,),
           CustomTextFormField(
+            controller: passwordController,
             validator: (value){}
           ),
           SizedBox(height: 60.h,),
