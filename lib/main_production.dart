44eecbc10b2d7b6e979f63dc7b0e2e2d43a885aa
tasneem-gamal <tasneemgamal044@gmail.com>
2024-12-01 18:01:50 -0800

    config firebase & init fastlane

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:soulsync/core/routing/app_router.dart';
 import 'package:soulsync/soul_sync.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   ScreenUtil.ensureScreenSize;
-  runApp(SoulSync(appRouter: AppRouter(),));
+  runApp(SoulSync(
+    appRouter: AppRouter(),
+  ));
 }
 
